4073613971e43c66345e1f52cbdcdbd1c742c598
albert patterson <apatterson189@gmail.com>
2019-07-07 17:55:11 -0400

    remove index and update build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,56 @@ const Server = require('karma').Server;
 const watch = require('gulp-watch');
 const connect = require('gulp-connect');
 
-gulp.task('test', (done) => {
-  new Server(
-      {
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true,
-      },
-      done)
-      .start();
-});
+gulp.task(
+    'test',
+    (done) => {
+      new Server(
+          {
+            configFile: __dirname + '/karma.conf.js',
+            singleRun: true,
+          },
+          done)
+          .start();
+    },
+);
 
-gulp.task('watch-test', () => watch(['src/**/*'], gulp.series('test')))
+gulp.task(
+    'watch-test',
+    () => watch(['src/**/*'], gulp.series('test')),
+)
 
-gulp.task('run', () => connect.server({root: 'src/main', livereload: true}));
+gulp.task('test-watch-test', gulp.series('test', 'watch-test'));
 
-gulp.task('reload', () => gulp.src(['src/main/*']).pipe(connect.reload()));
+gulp.task(
+    'start-server',
+    (done) => {
+      connect.server({
+        root: 'src/main',
+        livereload: true,
+        host: 'localhost',
+        port: '8080',
+      });
+      done();
+    },
+);
 
-gulp.task('watch-reload', () => watch(['src/**/*'], gulp.series('reload')));
+gulp.task(
+    'start-reload',
+    (done) => {
+      gulp.src(['src/main/*']).pipe(connect.reload());
+      done();
+    },
+);
 
-gulp.task('watch-run', gulp.parallel('run', 'watch-reload'));
+gulp.task(
+    'watch-reload',
+    (done) => {
+      watch(['src/**/*'], gulp.series('start-reload'));
+      done();
+    },
+);
+
+gulp.task(
+    'watch-run',
+    gulp.parallel('start-server', 'watch-reload'),
+);
